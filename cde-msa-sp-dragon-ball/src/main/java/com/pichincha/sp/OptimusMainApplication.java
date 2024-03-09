65015e1dfb551be67ba31f9c991d5b2bdd3a8859
krismaggot1991 <christian.muyon.rivera@gmail.com>
2024-03-09 10:03:59 -0500

    Init springboot project

---
+++ cde-msa-sp-dragon-ball/src/main/java/com/pichincha/sp/OptimusMainApplication.java
@@ -0,0 +1,33 @@
+package com.pichincha.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Arquitectura tradicional para el recurso onb-msa-sp-dragon-ball extendiendo
+ * de la interfaz generada.
+<br/>
+ * <b>Class</b>: PichinchaOptimusMainApplication<br/>
+ * <b>Copyright</b>: &copy; 2024 Banco Pichincha<br/>
+ *
+ * @author Banco Pichincha <br/>
+ * <u>Developed by</u>: <br/>
+ * <ul>
+*
+<li>Christian Muyon</li>
+*
+</ul>
+ * <u>Changes</u>:<br/>
+ * <ul>
+*
+<li>Mar 9, 2024 Creaci&oacute;n de Clase.</li>
+*
+</ul>
+ * @version 1.0
+ */
+@SpringBootApplication
+public class OptimusMainApplication {
+
+  public static void main(String[] args) {
+        SpringApplication.run(OptimusMainApplication.class, args);
+    }
+}
