26069b05b50779add158852a4406910f791036e1
krismaggot1991 <christian.muyon.rivera@gmail.com>
2024-03-10 22:09:59 -0500

    Specifc character adjustes

--- cde-spa-sp-dragon-ball/src/app/app.module.ts
+++ cde-spa-sp-dragon-ball/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { SharedModule } from './shared/shared.module';
 
     AppRoutingModule,
     SharedModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
